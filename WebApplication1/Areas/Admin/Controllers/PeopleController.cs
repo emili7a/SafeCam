93a519a18d888bc2cd2255a39ffbccf22e60a2e6
ClassTime <classtime@code.local>
2025-05-21 07:09:02 +0400

    bilet

--- WebApplication1/Areas/Admin/Controllers/PeopleController.cs
+++ WebApplication1/Areas/Admin/Controllers/PeopleController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication1.DataAccessLayer;
 using WebApplication1.Models;
 using WebApplication1.ViewModels.PersonViewModels;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace WebApplication1.Areas.Admin.Controllers
 {
@@ -15,9 +16,28 @@ namespace WebApplication1.Areas.Admin.Controllers
             var People = await _context.People.Select(x => new PersonGetVM() { FullName = x.FullName, ImageUrl = x.ImageUrl, profession = x.profession, Id = x.Id }).ToListAsync();
             return View(People);
         }
-        public IActionResult Create()
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PersonCreateVM vm)
         {
-            return View();
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            if(vm.Image.Length*1024*1024>2)
+            {
+                ModelState.AddModelError("Image", "Image 2 mb-dan cox ola bilmez.");
+                    return View(vm);
+            }
+            if (!vm.Image.Contenttype.Contains("image"))
+            {
+                ModelState.AddModelError("image", "yalniz sekil formatinda sekil daxil ede bilersiniz.");
+            }
+
         }
+       
+
+            
     }
 }
---
+++ WebApplication1/ViewModels/PersonViewModels/PersonCreateVM.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.ViewModels.PersonViewModels
+{
+    public class PersonCreateVM
+    {
+        public string Image { get; internal set; }
+        public int Id { get; set; }
+    }
+}
